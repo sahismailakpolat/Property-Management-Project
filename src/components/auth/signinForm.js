0381bb1ea9b6af4de66001ab3bc09f42e64c19bb
akpolat.sahismail@gmail.com <akpolat.sahismail@gmail.com>
2018-10-10 11:36:19 -0500

    login field component, textlink component and remaining sigin styles

--- src/components/auth/signinForm.js
+++ src/components/auth/signinForm.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
 
 import { FormTitle } from '../formTitle';
-import { FormInput } from '../formFields';
+import { FormInput, FormButton } from '../formFields';
+import TextLink from './textLink';
 
 
 class SigninForm extends Component {
@@ -28,6 +29,18 @@ class SigninForm extends Component {
                     component={FormInput}
 
                 />
+                <Field
+                    className='sign-in-form__login'
+                    name='Login'
+                    type='submit'
+                    title='Login'
+                    component={FormButton}
+
+                />
+                <div className='sign-in-form__text-links'>
+                    <TextLink to='/forgot' text='Forgot Password' />
+                    <TextLink to='/signup' text='Not a member? Register here' />
+                </div>
             </form>
         )
 
