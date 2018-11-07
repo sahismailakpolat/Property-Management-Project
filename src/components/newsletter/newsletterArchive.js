44b6e6f90ab22d51a9cd866011fd114d481ae8b8
akpolat.sahismail@gmail.com <akpolat.sahismail@gmail.com>
2018-11-07 13:06:44 -0600

    styled archive items and such

--- src/components/newsletter/newsletterArchive.js
+++ src/components/newsletter/newsletterArchive.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 function ArchiveItem({ title, date }) {
     return (
-        <div className='archive-item'>
+        <div className='archive-item archive-items__item'>
             <div className='archive-item__title'>{title}</div>
             <div className='archive-item__date'>{date}</div>
 
@@ -15,8 +15,12 @@ class NewsletterArchive extends Component {
         return (
             <div className='newsletter-archive'>
                 <div className='newsletter-archive__title'>Archive</div>
-                <div className='newsletter-archive__items'>
+                <div className='newsletter-archive__items archive-items'>
                     <ArchiveItem title='Allah' date='Sen buyuksun Allahim' />
+                    <ArchiveItem title='Allah' date='Sen buyuksun Allahim' />
+                    <ArchiveItem title='Allah' date='Sen buyuksun Allahim' />
+                    <ArchiveItem title='Allah' date='Sen buyuksun Allahim' />
+
                 </div>
 
             </div>
