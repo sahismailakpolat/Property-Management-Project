4857a19d4bc7d312f1e7df1d2a4079713e61658f
akpolat.sahismail@gmail.com <akpolat.sahismail@gmail.com>
2018-11-08 03:24:50 -0600

    parsed date for archive item component

--- src/components/newsletter/newsletterArchive.js
+++ src/components/newsletter/newsletterArchive.js
@@ -4,7 +4,15 @@ function ArchiveItem({ title, date }) {
     return (
         <div className='archive-item archive-items__item'>
             <div className='archive-item__title'>{title}</div>
-            <div className='archive-item__date'>{date}</div>
+            <div className='archive-item__date'>
+
+                {date.getMonth() + 1}
+                /
+                {date.getDate()}
+                /
+                {date.getFullYear() - 2000}
+
+            </div>
 
         </div>
     )
@@ -16,10 +24,10 @@ class NewsletterArchive extends Component {
             <div className='newsletter-archive'>
                 <div className='newsletter-archive__title'>Archive</div>
                 <div className='newsletter-archive__items archive-items'>
-                    <ArchiveItem title='Allah' date='Sen buyuksun Allahim' />
-                    <ArchiveItem title='Allah' date='Sen buyuksun Allahim' />
-                    <ArchiveItem title='Allah' date='Sen buyuksun Allahim' />
-                    <ArchiveItem title='Allah' date='Sen buyuksun Allahim' />
+                    <ArchiveItem title='Allah' date={new Date()} />
+                    <ArchiveItem title='Allah' date={new Date()} />
+                    <ArchiveItem title='Allah' date={new Date()} />
+                    <ArchiveItem title='Allah' date={new Date()} />
 
                 </div>
 
