a84ba753e17045f20eb33b771885949b292f2c92
akpolat.sahismail@gmail.com <akpolat.sahismail@gmail.com>
2018-11-08 04:57:15 -0600

    styled newsletter lastest and applied row gap to the newsletter grid

--- src/components/newsletter/newsletterLatest.js
+++ src/components/newsletter/newsletterLatest.js
@@ -8,7 +8,7 @@ class NewsletterLatest extends Component {
                 <h1 className='newsletter-latest__title'>Title goes here</h1>
                 <img className='newsletter-latest__image' src='http://via.placeholder.com/960x258' />
                 <div className='newsletter-latest__body'>
-                    <p>The Allah is greatest!</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                 </div>
 
             </div>
