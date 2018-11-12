63ef6871dcfa7d453fef8e9f6b8c22af6169c031
akpolat.sahismail@gmail.com <akpolat.sahismail@gmail.com>
2018-11-12 04:52:50 -0600

    built newsletterNewForm

--- src/components/newsletter/newsletterNew.js
+++ src/components/newsletter/newsletterNew.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
-import SigninForm from '../auth/signinForm';
+
+import NewNewsletterForm from './newsletterNewForm';
 
 class NewNewsletter extends Component {
+
     onSubmit = (fields) => {
-        console.log('trying to handle submit');
+        console.log('trying to handle submit')
     }
 
     render() {
         return (
             <div className='new-newsletter'>
-                <SigninForm onSubmit={(event) => this.onSubmit(event)} />
-
+                <NewNewsletterForm onSubmit={(event) => this.onSubmit(event)} />
             </div>
         )
     }
