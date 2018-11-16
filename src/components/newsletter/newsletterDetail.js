3d848ae4ca52497eb8f714970289a38cb966bffe
akpolat.sahismail@gmail.com <akpolat.sahismail@gmail.com>
2018-11-16 06:59:54 -0600

    fetched newsletter with id for details

--- src/components/newsletter/newsletterDetail.js
+++ src/components/newsletter/newsletterDetail.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
-
+import { connect } from 'react-redux';
+import * as actions from '../../actions';
 
 class NewsletterDetail extends Component {
+    componentDidMount() {
+        this.props.fetchNewsletterWithId(this.props.match.params.id);
+    }
+
     render() {
+        console.log(this.props.newsletterToEdit);
         return (
             <div className='newsletter-detail'>
                 Newsletter Detail
@@ -11,4 +17,9 @@ class NewsletterDetail extends Component {
     }
 }
 
-export default NewsletterDetail;
+function mapStateToProps(state) {
+    const { newsletterToEdit } = state.newsletters
+    return { newsletterToEdit };
+}
+
+export default connect(mapStateToProps, actions)(NewsletterDetail);
