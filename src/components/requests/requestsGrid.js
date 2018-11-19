b25bd4aceeed6c3c13e9c161801f7bba4a67d871
akpolat.sahismail@gmail.com <akpolat.sahismail@gmail.com>
2018-11-19 11:29:01 -0600

    built add requests button and routed new request form

--- src/components/requests/requestsGrid.js
+++ src/components/requests/requestsGrid.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
+import Button from '../button';
 
 class RequestsGrid extends Component {
+
+    handleAddNewsletter = () => {
+        this.props.history.push('/requests/new');
+    }
+
     render() {
         return (
             <div className='requests-grid'>
+                <Button className='requests-grid__button' icon='fas fa-plus' callback={() => this.handleAddNewsletter()} />
 
             </div>
         )
