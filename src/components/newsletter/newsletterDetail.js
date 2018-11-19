fe8c13eff6fa29d7725baee7f8ccf9c62b6de108
akpolat.sahismail@gmail.com <akpolat.sahismail@gmail.com>
2018-11-19 10:32:28 -0600

    cancel button for newsletter archive component

--- src/components/newsletter/newsletterDetail.js
+++ src/components/newsletter/newsletterDetail.js
@@ -5,6 +5,8 @@ import * as actions from '../../actions';
 import NewsletterBox from './newsletterBox';
 import NewsletterLatest from './newsletterLatest';
 
+import { FormTitle } from '../formTitle';
+import { FormButton } from '../formFields';
 
 class NewsletterDetail extends Component {
     componentDidMount() {
@@ -15,9 +17,15 @@ class NewsletterDetail extends Component {
         console.log(this.props.newsletterToEdit);
         return (
             <div className='newsletter-detail'>
+                <FormTitle className='newsletter-detail__title' text='Newsletter Archive' />
                 <NewsletterBox date={this.props.newsletterToEdit.date} />
                 <NewsletterLatest {...this.props.newsletterToEdit} />
-
+                <FormButton
+                    className='newsletter-detail__cancel'
+                    small={true}
+                    onClick={() => this.props.history.push('/dashboard')}
+                    title='Cancel'
+                />
             </div>
         )
     }
