c0fd1189c2f97d2835cf2e92ee3eab2a31e3d112
akpolat.sahismail@gmail.com <akpolat.sahismail@gmail.com>
2018-11-21 14:18:11 -0600

    cleaned up request item grid

--- src/components/requests/requestsItem.js
+++ src/components/requests/requestsItem.js
@@ -10,16 +10,17 @@ class RequestsItem extends Component {
             <div className='requests-item'>
                 <Icon className='requests-item__icon' icon='fas fa-exclamation-triangle' />
                 <div className='requests-item__title'>
-                    Yo my door fell down
+                    <div className='requests-item__title__text'>Yo my door fell down</div>
+                    <Icon className='requests-item__title__arrow' icon='fas fa-sort-down' />
+
                 </div>
                 <div className='requests-item__tenant-unit'>
                     Max - Unit 115
                 </div>
-                <Icon className='requests-item__arrow' icon='fas fa-sort-down' />
                 <div className='requests-item__date'>
                     09/15/97
                 </div>
-                <Button className='requests-icon__move' icon='fas fa-wrench' callback={() => console.log('tryna change request status.')} />
+                <Button className='requests-item__move' icon='fas fa-wrench' callback={() => console.log('tryna change request status.')} />
                 <div className='requests-item__description'>
                     <img
                         className='requests-item__description-img'
