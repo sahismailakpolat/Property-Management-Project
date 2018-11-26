d932d97c351c7d97ba3339eaad7bb42dd7894d0f
akpolat.sahismail@gmail.com <akpolat.sahismail@gmail.com>
2018-11-26 11:35:27 -0600

    put correct item count in boxes for request feature

--- src/components/requests/requestsBoxes.js
+++ src/components/requests/requestsBoxes.js
@@ -5,14 +5,36 @@ class RequestsBoxes extends Component {
     render() {
         return (
             <div className='requests-boxes'>
-                <RequestsBox title={'pending'} count={2} />
-                <RequestsBox title={'in-progress'} count={1} />
-                <RequestsBox title={'complete'} count={5} />
+                <RequestsBox title={'pending'} count={this.props.pendingCount} />
+                <RequestsBox title={'in-progress'} count={this.props.progressCount} />
+                <RequestsBox title={'complete'} count={this.props.completeCount} />
 
 
             </div>
         )
     }
 }
+function mapStateToProps(state) {
+    const { requests } = state.requests
 
-export default RequestsBoxes;
+    var pendingCount = 0;
+    var progressCount = 0;
+    var completeCount = 0;
+
+    requests.map(request => {
+        if (request.status == 'pending') {
+            pendingCount += 1;
+        } else if (request.status == 'progress') {
+
+        } else if (request.status == 'complete') {
+            completeCount += 1;
+        }
+    })
+    return {
+        pendingCount,
+        progressCount,
+        completeCount
+    }
+}
+
+RequestsBoxes = connect(mapStateToProps)(RequestsBoxes);
