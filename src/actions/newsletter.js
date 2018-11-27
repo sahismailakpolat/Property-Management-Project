7eae31c2cefd2a5a7ac0adfe1f9d3c806c5fbc46
akpolat.sahismail@gmail.com <akpolat.sahismail@gmail.com>
2018-11-27 06:13:06 -0600

    refactored fetchnewsletters to fetch from the backend

--- src/actions/newsletter.js
+++ src/actions/newsletter.js
@@ -3,30 +3,26 @@ import {
     FETCH_NEWSLETTER_ID
 } from './types';
 
+import axios from 'axios';
+import { ROOT_URL } from '../config';
+
 export function fetchNewsletters() {
-    const response = {
-        data: [
-            {
-                _id: '1993',
-                title: 'Happy Holiday Fam',
-                body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-                date: new Date(),
-                imageUrl: 'http://via.placeholder.com/960x258'
-            },
-            {
-                _id: '935',
-                title: 'Second Newsletter',
-                body: 'Lorem ip dolorbfbfbfb sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut hfnggymum quis nostrud emmfmfmxercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-                date: new Date(),
-                imageUrl: 'http://via.placeholder.com/960x258'
-            }
-        ]
+    return function (dispatch) {
+        const token = localStorage.getItem('token');
+        const headers = { headers: { authorization: token } }
+        axios.get(`${ROOT_URL}/newsletters`, headers)
+            .then(response => {
+                dispatch({
+                    type: SET_NEWSLETTERS,
+                    payload: response.data
+                })
+
+            })
+            .catch(err => {
+                console.log(err);
+            })
     }
 
-    return {
-        type: SET_NEWSLETTERS,
-        payload: response.data
-    }
 }
 
 export function fetchNewsletterWithId(id) {
