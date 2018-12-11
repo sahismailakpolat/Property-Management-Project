e7bd8c581e6c1df392cd73207a8e919f2ca440f0
akpolat.sahismail@gmail.com <akpolat.sahismail@gmail.com>
2018-12-11 14:29:26 -0600

    hooked up layout to mapstatetoprops

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,20 +1,25 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
 import { Header, HeaderBar } from './header';
 
 class Layout extends Component {
-
-
-
     render() {
         return (
             <div className="layout-grid">
-                <Header />
+                <Header
+                    title='Welcome to HOA Manager!'
+                    subtitle='Please login to continue'
+                />
                 <HeaderBar />
                 {this.props.children}
             </div>
         )
     }
 }
+function mapStateToProps(state) {
+    return state;
+}
 
-
+Layout = connect(mapStateToProps)(Layout);
 export default Layout;
